7c240335be1ca5f0eb119962b5afc85c320a2aeb
Patrick Altman <paltman@gmail.com>
2013-08-31 23:52:55 -0500

    Update for bootstrap 3.0 changes

--- django_forms_bootstrap/templatetags/bootstrap_tags.py
+++ django_forms_bootstrap/templatetags/bootstrap_tags.py
@@ -9,6 +9,10 @@ register = template.Library()
 @register.filter
 def as_bootstrap(form):
     template = get_template("bootstrap/form.html")
+    for field in form.fields:
+        name = form.fields[field].widget.__class__.__name__.lower()
+        if not name.startswith("radio") and not name.startswith("checkbox"):
+            form.fields[field].widget.attrs["class"] = " form-control"
     c = Context({"form": form})
     return template.render(c)
 
